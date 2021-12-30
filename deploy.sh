84a83dcb66920fab74aa55be7c88d47fb9a3faf8
Mahan Lamee <mahan.lameie87@gmail.com>
2021-12-30 20:43:27 +0330

    Completed deploy script + removed excessive files

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,27 @@
 #!/bin/sh
+
 sudo pacman -Syu &&
 sudo pacman -S --needed - < ./package_list_pacman &&
+
 git clone https://aur.archlinux.org/yay.git &&
 cd yay &&
 makepkg -si &&
 cd .. &&
 sudo rm -r yay &&
+
 yay -S --needed - < ./package_list_aur &&
 
+files=($(find . -type f -printf "\"%p\"\n" | tr '\n' ' '))
+delete=(deploy.sh deploy.py package_list_pacman package_list_aur path.json)
+
+for del in ${delete[@]}
+do
+    files=("${array[@]/$del}")
+done
+
+for file in $files
+do
+    sudo cp -a $file $(echo $file | sed 's|"./|"/|g' | sed "s|/home/user|/home/$(whoami)|g")
+done
+
+./post-install.sh
